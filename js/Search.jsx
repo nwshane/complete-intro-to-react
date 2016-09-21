ddfbcc2f66f5ce52ba26fee5b9b2c0e91be9501d
nwshane <django09@gmail.com>
2016-09-21 10:12:41 +0400

    Made search input set searchTerm state of Search component.

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,16 +3,32 @@ const data = require('../public/data')
 
 const ShowCard = require('./ShowCard')
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {
-        data.shows.map((show) => (
-          <ShowCard {...show} key={show.imdbID} />
-        ))
-      }
-    </div>
-  </div>
-)
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is my search term'
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>{this.state.searchTerm}</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+        </header>
+        <div className='shows'>
+          {
+            data.shows.map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+})
 
 module.exports = Search
