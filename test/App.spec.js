943e5c9e7269867cb2541220d42012d1cb5435ed
nwshane <django09@gmail.com>
2016-09-24 21:02:50 +0400

    Added first real spec to test that Search renders the brand.

--- test/App.spec.js
+++ test/App.spec.js
@@ -2,10 +2,14 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const React = require('react')
+const Search = require('../js/Search')
+const { shallow } = require('enzyme')
 
 // Documents that you're testing the Search component
 describe('<Search />', () => {
-  it('should pass', () => {
-    expect(1 + 1).to.equal(2)
+  it('should render the brand', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
   })
 })
