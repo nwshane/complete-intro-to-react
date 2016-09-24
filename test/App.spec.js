7299b95b3805ceae6f3f5bcfe63dac626f7fdc58
nwshane <django09@gmail.com>
2016-09-24 23:06:59 +0400

    Added more tests for the Search component.

--- test/App.spec.js
+++ test/App.spec.js
@@ -4,12 +4,28 @@
 const { expect } = require('chai')
 const React = require('react')
 const Search = require('../js/Search')
-const { shallow } = require('enzyme')
+const ShowCard = require('../js/ShowCard')
+const { mount, shallow } = require('enzyme')
+const { shows } = require('../public/data')
 
 // Documents that you're testing the Search component
 describe('<Search />', () => {
   it('should render the brand', () => {
     const wrapper = shallow(<Search />)
     expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
+  })
+
+  it('should render as many shows as there are data for', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(ShowCard).length).to.equal(shows.length)
+  })
+
+  it('should filter correctly given new state', () => {
+    const wrapper = mount(<Search />)
+    const input = wrapper.find('.search-input')
+    input.node.value = 'house'
+    input.simulate('change')
+    expect(wrapper.state('searchTerm')).to.equal('house')
+    expect(wrapper.find('.show-card').length).to.equal(2)
   })
 })
