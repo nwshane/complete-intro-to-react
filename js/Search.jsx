bc565215981b3ea32c22378771d032c641402fc6
nwshane <django09@gmail.com>
2016-09-24 11:09:19 +0400

    Set up search to filter show cards.

--- js/Search.jsx
+++ js/Search.jsx
@@ -6,7 +6,7 @@ const ShowCard = require('./ShowCard')
 const Search = React.createClass({
   getInitialState () {
     return {
-      searchTerm: 'this is my search term'
+      searchTerm: ''
     }
   },
   handleSearchTermEvent (event) {
@@ -16,14 +16,20 @@ const Search = React.createClass({
     return (
       <div className='container'>
         <header className='header'>
-          <h1 className='brand'>{this.state.searchTerm}</h1>
+          <h1 className='brand'>svideo</h1>
           <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
         </header>
         <div className='shows'>
           {
-            data.shows.map((show) => (
-              <ShowCard {...show} key={show.imdbID} />
-            ))
+            data.shows
+              .filter((show) => {
+                return `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+              })
+              .map((show) => (
+                <ShowCard {...show} key={show.imdbID} />
+              ))
           }
         </div>
       </div>
